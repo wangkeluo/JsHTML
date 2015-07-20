c4668137a9e7b4746b35dde27f1c43fd95c04269
Corot <corot@leansoftware.com.hk>
2015-07-21 00:26:24 +0800

    Modify comment layout

--- dropbox.js
+++ dropbox.js
@@ -1,6 +1,6 @@
 (function dbx() {
 	console.log("============================================================");
-	console.log("======Step 1: Create an application and dbox object=========");
+	console.log("Step 1: Create an application and dbox object===============");
 
 	var open = require("open");
 	var dbox = require("dbox");
@@ -12,22 +12,23 @@
 	console.log(app);
 
 	console.log("============================================================");
-	console.log("======Step 2:Authorization->Get request token===============");
+	console.log("Step 2:Authorization->Get request token=====================");
 
 	app.requesttoken(function(status, request_token) {
 		console.log(request_token);
 		var auth_url = request_token['authorize_url'];
 		console.log("============================================================");
-		console.log("============Step 3:User authenticate your app===============");
+		console.log("Step 3:User authenticate your app===========================");
 		var browser = open(auth_url);
 		setTimeout(function() {
 			console.log("*****success authenticate*****");
 			console.log("============================================================");
-			console.log("==========Step 4:Authorization->Get access token============");
+			console.log("Step 4:Authorization->Get access token======================");
 			app.accesstoken(request_token, function(status, access_token) {
 				console.log(access_token);
 				var client = app.client(access_token);
-				//console.log(client);
+			console.log("============================================================");
+			console.log("Step 5:Get confidential user info===========================");
 				client.account(function(status, reply) {
 					console.log(reply)
 				});
